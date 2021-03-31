2160418a215f466204e966cf81f6a5956bd3009b
jiiaatn <neojiatian@gmail.com>
2021-03-31 14:44:07 +0800

    Display recycled items on IdentifiedItem page

--- src/components/IdentifiedItem.vue
+++ src/components/IdentifiedItem.vue
@@ -1,26 +1,64 @@
 <template>
   <div id="content">
-    <p>Item recycled: </p>
-    <p>Points Earned: </p>
+    <div v-for="items in recycledData" v-bind:key="items[0][0]">
+      <div v-for="item in items" v-bind:key="item[0]">
+        <div v-for="(value, index) in item" v-bind:key="index" id='display'>
+          <img src="../assets/glass.jpg" width="200" height="200" v-if="index=='Glass'"/>
+          <img src="../assets/plastic.jpg" width="200" height="200" v-if="index=='Plastic'"/>
+          <img src="../assets/paper.jpg" width="200" height="200" v-if="index=='Paper'"/>
+          <img src="../assets/metal.jpg" width="200" height="200" v-if="index=='Metal'"/>
+          <p>Item recycled: {{index}}</p>
+          <p>Points Earned: {{value}}</p>
+        </div>
+      </div>
+    </div>
     <button v-on:click="directed()">Back to Home</button>
   </div>
 </template>
 
 <script>
-//import Header from './components/Header.vue'
+//import db from "../firebase.js"
+
 export default {
+  data() {
+    return {
+      email: "",
+      recycledData: [],
+    }
+  },
   methods: {
-      directed: function() {
-        this.$router.push({ path: '/Home' })
+      directed: function() {        
+        this.$router.push({name:"Home", params:{ userEmail: this.email }})
       },
       getData: function() {
-        var data = this.$route.params.data;
-        console.log("data: " + data);
-        this.recycledData = data
+        var data = JSON.parse(this.$route.params.data);
+        console.log(data);
+        this.recycledData.push(data);
+        console.log(this.recycledData);
+      },
+      getUser: function() {
+        this.email = this.$route.params.userEmail;
+        console.log("email: " + this.email);
+      },
+      addToDB: function() { // add newly recycled information into recycling history
+        // Get today's date
+        var dateTime = new Date();
+        var month = ('0' + (dateTime.getMonth() + 1)).slice(-2);
+        var date = ('0' + dateTime.getDate()).slice(-2);
+        var year = dateTime.getFullYear();
+        var dateTimeNewFormat = date + '-' + month + '-' + year;
+        console.log(dateTimeNewFormat);
+        
+        // Push to database
+        // Need to retrieve from db and check for same dates and combine (recycling more than once a day)
+        //db.collection(this.email).doc("Recycling history").update({ 
+          //dateTimeNewFormat: this.recycledData[0]}); 
       }
   },
   created() {
     this.getData();
+    this.getUser();
+    this.addToDB();
   }
 }
 </script>
@@ -35,5 +73,8 @@ button {
 #content {
     background-color: #E8E1CF;
     padding: 50px;
+}
+#display {
+  margin: 50px;
 }
 </style>
