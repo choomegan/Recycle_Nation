5f935d96b4ce56edfe496a1f33f720b41dc336e0
Megan <choomegan@gmail.com>
2021-04-02 16:28:14 +0800

    Created table for recycling history

--- src/components/RecyclingHistory.vue
+++ src/components/RecyclingHistory.vue
@@ -1,16 +1,73 @@
 <template>
-  <div>
-    
+  <div id="history">
+    <table id="table">
+      <thead> 
+        <tr> 
+          <th>Date/Time</th> 
+          <th>Name of Item</th> 
+          <th>Points Earned</th>
+        </tr> 
+      </thead> 
+    </table> 
   </div>
 </template>
 
 <script>
-//import Header from './components/Header.vue'
+import firebase from 'firebase/app'
+import db from '../firebase.js'
+
 export default {
-  
+    data() {
+      
+    },
+    methods: {
+        retrieveData: function() {
+            var user = firebase.auth().currentUser;
+            console.log("hello")
+            db.collection(user.email).doc("Recycling history").get().then(doc => {
+              console.log(doc.data()) //
+              console.log("doc.data()") //
+              Object.values(doc.data()).forEach(item => {
+                console.log(item)
+                console.log("item")
+                let row = `<tr>
+                              <td>${item.Date} ${item.Time}</td>
+                              <td>${item.Item}</td> 
+                              <td>${item.Points}</td> 
+                          <tr>`
+                let table = document.getElementById("table")
+                table.innerHTML += row 
+              })
+            })
+      }
+    },
+    created() {
+      this.retrieveData();
+    }
 }
 </script>
 
-<style>
+<style scoped>
+#table{
+    text-align: center;
+    border-collapse: collapse;
+    margin: 25px 0;
+    font-size: 0.9em;
+    font-family: sans-serif;
+    min-width: 400px;
+    align-content: center ;
+}
 
+#table thead tr {
+    background-color: #009879;
+    color: #ffffff;
+    text-align: left;
+}
+
+th, td{
+  padding: 12px
+}
+#history {
+  align-content: center;
+}
 </style>
