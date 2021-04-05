25df94f06dd75f8a6ee7c5ea548e868547e7c26d
oleeviea <oliviama2000@gmail.com>
2021-04-06 07:41:13 +0800

    Added QR feature (generating and scanning)

---
+++ src/components/GenerateQR.vue
@@ -0,0 +1,12 @@
+<template>
+  <vue-qrcode value='localhost:8080/Login/[{"item":"glass"}, {"points":40}]' />
+</template>
+<script>
+import VueQrcode from 'vue-qrcode'
+ 
+export default {
+  components: {
+    VueQrcode,
+  },
+}
+</script>
