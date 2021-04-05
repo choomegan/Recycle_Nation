3196300a91fb96693e00d60d99cd4e8f6c7b65d3
oleeviea <oliviama2000@gmail.com>
2021-04-06 02:33:07 +0800

    added delete rewards option at My Rewards

--- src/components/MyRewards.vue
+++ src/components/MyRewards.vue
@@ -21,7 +21,7 @@
                 <div class="start" v-if="myRewards.length==0">
                     You have no existing rewards.
                 </div>
-                <div v-for="item in myRewards" v-bind:key="item.title">
+                <div v-for="item in myRewards" v-bind:key="item.code">
                     <div class="block">
                         <img class="left" v-if="item.title == 'GrabFood'" v-bind:src="item.image"/>
                         <img class="left" v-if="item.title == 'GrabGifts'" v-bind:src="item.image"/>
@@ -36,7 +36,7 @@
                             <br>{{item.code}}
                         </div>
                         <div id="cross">
-                            <button> X </button>
+                            <button v-on:click="deleteReward(item)"> X </button>
                         </div>
                     </div>
                     <br>
@@ -130,13 +130,29 @@ export default {
             })
         },
         success(item) {
-            item.code = this.displayCode(item);
-            this.myRewards.push(item);
             if (item.title =="tree"){
                 alert("Congratulations you have planted a tree!")
             } else if (item.title == "GrabFood") {
+                item = {
+                    title: 'GrabFood',
+                    name: "$10 GrabFood Voucher",
+                    image: require('../assets/grabFood.png'),
+                    points: 500,
+                    code: "",
+                }
+                item.code = this.displayCode(item);
+                this.myRewards.push(item);
                 alert("Congratulations you have redeemed a GrabFood Voucher! You can now view the voucher and redemption code at [My Rewards]")
             } else if (item.title == "GrabGifts") {
+                item = {
+                    title: 'GrabGifts',
+                    name: "$10 GrabGifts Voucher",
+                    image: require('../assets/grabGifts.png'),
+                    points: 500,
+                    code: "",
+                }
+                item.code = this.displayCode(item);
+                this.myRewards.push(item);
                 alert("Congratulations you have redeemed a GrabGifts Voucher! You can now view the voucher and redemption code at [My Rewards]")
             } else if (item.title == "Donate") {
                 alert("Congratulations you have donated $1!")
@@ -144,16 +160,28 @@ export default {
         },
         displayCode: function(item) {
             var code;
+            if (item.code != "") {
+                return item.code;
+            }
             if (item.title == "GrabFood") {
-                code = "Grab Code: RNGF10" 
+                code = "Grab Code: GF10" 
             } else if (item.title == "GrabGifts") {
-                code = "Grab Code: RNGG10"
+                code = "Grab Code: GG10"
             }
             let chars = "abcdefghijklmnopqrstuvwxyz"
             for( let i=0; i < 3; i++ ) {
                 code += chars.charAt(Math.floor(Math.random() * chars.length))
             }
             return code.toUpperCase();
+        },
+        deleteReward: function(item) {
+            var index;
+            for (index = 0; index < this.myRewards.length; index++) {
+                if (item.code == this.myRewards[index].code) {
+                    this.myRewards.splice(index, 1);
+                    break;
+                }
+            }
         }
     },
 }
@@ -183,19 +211,18 @@ export default {
 }
 .side {
     background-color:oldlace;
-    padding: 5px;
     width: 300px;
 }
 .start {
-    width: 400px;
+    width: 430px;
 }
 .block {
     display: flex;
     flex-direction: row;
     flex-flow: wrap;
-    justify-content:flex-start;
+    justify-content:space-evenly;
     width:430px;
-    padding: 20px 0px 20px 20px;
+    padding: 20px 10px 20px 30px;
     background-color:white;
 }
 .right {
